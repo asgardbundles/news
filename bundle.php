b63bf76b88bf74be6f9427cda633a6517756bffa
unknown <Michel>
2013-11-17 18:55:47 +1100

    fixes

--- bundle.php
+++ bundle.php
@@ -2,13 +2,9 @@
 namespace App\News;
 
 class Bundle extends \Coxis\Core\BundleLoader {
-	public function load($queue) {
-		parent::load();
-	}
-
 	public function run() {
-		\Coxis\Admin\Libs\AdminMenu::$menu[0]['childs'][] = array('label' => 'Actualites', 'link' => 'actualites');
-		\Coxis\Admin\Libs\AdminMenu::$home[] = array('img'=>\URL::to('actualite/icon.svg'), 'link'=>'actualites', 'title' => __('News'), 'description' => __('Les actualités'));
+		\Coxis\Admin\Libs\AdminMenu::instance()->menu[0]['childs'][] = array('label' => 'Actualites', 'link' => 'actualites');
+		\Coxis\Admin\Libs\AdminMenu::instance()->home[] = array('img'=>\URL::to('actualite/icon.svg'), 'link'=>'actualites', 'title' => __('News'), 'description' => __('Les actualités'));
 		parent::run();
 	}
 }
