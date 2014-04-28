e5e1992aa58377f4ce3acc88c4507a7551cc55e8
Michel Hognerud <leyou.m@gmail.com>
2014-04-28 11:27:06 +0800

    fixes

--- asgard-install.php
+++ asgard-install.php
@@ -4,7 +4,7 @@ require_once _VENDOR_DIR_.'autoload.php'; #composer autoloader
 \Asgard\Core\App::loadDefaultApp();
 
 \Asgard\Utils\FileManager::copy(__DIR__.'/app/news', _DIR_.'app/news');
-\Asgard\Utils\FileManager::copy(__DIR__.'/tests/news', _DIR_.'tests/news');
+\Asgard\Utils\FileManager::copy(__DIR__.'/tests/ActualiteAdminTest.php', _DIR_.'tests/ActualiteAdminTest.php');
 \Asgard\Utils\FileManager::copy(__DIR__.'/web/news', _DIR_.'web/news');
 
 \Asgard\Orm\Libs\MigrationsManager::addMigrationFile(__DIR__.'/migrations/News.php');
